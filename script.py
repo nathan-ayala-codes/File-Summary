b909d4f224855b36efb71ad52ebf396e74411d89
Nathan Ayala <nathanaa2006@gmail.com>
2026-02-03 15:37:44 -0800

    - Added Error Bound for file not being read. - Added Error messsages - Added Loop to keep asking for file name if error occurs - Added comments explaining get_data function

--- script.py
+++ script.py
@@ -1,4 +1,8 @@
+
 def get_data(f):
+    # Sets local variables to look through the given file and then returns the values found.
+    # When called must have three areas to store values in!
+    # Pass in the file as parameter.
     number_of_lines = 0
     number_of_words = 0
     number_of_characters = 0
@@ -12,14 +16,20 @@ def get_data(f):
     return number_of_lines, number_of_words, number_of_characters
 
 
-with open(input("Enter in a file name here:")) as f:
-    number_of_lines, number_of_words, number_of_characters = get_data(f)
+File_not_read = True # Flag for failure to read file
 
-
-print (f"Hello User!")
-print(f"After opening your file we found that you have,")
-print(f"{number_of_lines} lines of data in your file.")
-print(f"You also have {number_of_words} words in your file,")
-print(f"That is {number_of_characters} characters!")
+while File_not_read:
+    try:
+        with open(input("Enter in a file name here:")) as f:
+            number_of_lines, number_of_words, number_of_characters = get_data(f)
+    except:
+        print("Error: The file either doesn't exist or cannot be read :(, please try again")
+    else:
+        print(f"Hello User!")
+        print(f"After opening your file we found that you have,")
+        print(f"{number_of_lines} lines of data in your file.")
+        print(f"You also have {number_of_words} words in your file,")
+        print(f"That is {number_of_characters} characters!")
+        break
 
 f.close()
